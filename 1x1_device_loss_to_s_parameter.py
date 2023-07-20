30cbfef3447cd535583541c3720df6504c4f1ede
M. Jobayer Hossain <59855260+jobayer07@users.noreply.github.com>
2023-07-19 21:56:20 -0400

    Update 1x1_device_loss_to_s_parameter.py
    Completed the script

--- 1x1_device_loss_to_s_parameter.py
+++ 1x1_device_loss_to_s_parameter.py
@@ -3,17 +3,58 @@ import numpy as np
 
 portnames = ['opt_1_fbr','opt_2_fn']
 portlocs  = ['LEFT', 'RIGHT']
-
-N=76 #wavelength points
 polarization='TE'
 mode_id=1
 
-#file_object  = open("filename", "mode")
-f= open("first_s.txt","w+")
+def convert_to_s_parameter(loss_model):
+    np.set_printoptions(suppress=True, formatter={'float_kind':'{:f}'.format})
+    f=3e8/lam
+    T=10**(-loss_model/20) #dB=20log(x), here 20 is for power
+    T_phase=T-T
+    
+    #Construct S parameters: S12=S21, S11=S22
+    N=len(loss_model)
+    S21=np.zeros([N, 3], dtype=float)
+    S21[:, 0]=f
+    S21[:, 1]=T
+    S21[:, 2]=T_phase
+    
+    S12=S21
+    
+    S11=np.zeros([N, 3], dtype=float)
+    S11[:, 0]=f
+    S11[:, 1]=T-T
+    S11[:, 2]=T_phase
+    
+    S22=S11
+    
+    #file_object  = open("filename", "mode")
+    txt_file= open(str(name) +".txt","w+")
+    
+    #S11
+    txt_file.write('[\"' + portnames[0] + '\",\"' + portlocs[0] + '\"]\n')
+    txt_file.write('[\"' + portnames[1] + '\",\"' + portlocs[1] + '\"]\n')
+    txt_file.write('(\"' + portnames[0] + '\",\"' +str(polarization)+ '\",' +str(mode_id) + ',\"' + portnames[0] +'\",' + str(mode_id) +',\"transmission\")\n')
+    txt_file.write('('+ str(N) +',3)\n')
+    txt_file.write(re.sub('( \[|\[|\])', '', str(S11)))
+    
+    #S21
+    txt_file.write('\n(\"' + portnames[1] + '\",\"' +str(polarization)+ '\",' +str(mode_id) + ',\"' + portnames[0] +'\",' + str(mode_id) +',\"transmission\")\n')
+    txt_file.write('('+ str(N) +',3)\n')
+    txt_file.write(re.sub('( \[|\[|\])', '', str(S21)))
+    
+    #S22
+    txt_file.write('\n(\"' + portnames[1] + '\",\"' +str(polarization)+ '\",' +str(mode_id) + ',\"' + portnames[1] +'\",' + str(mode_id) +',\"transmission\")\n')
+    txt_file.write('('+ str(N) +',3)\n')
+    txt_file.write(re.sub('( \[|\[|\])', '', str(S22)))
+    
+    #S12
+    txt_file.write('\n(\"' + portnames[0] + '\",\"' +str(polarization)+ '\",' +str(mode_id) + ',\"' + portnames[1] +'\",' + str(mode_id) +',\"transmission\")\n')
+    txt_file.write('('+ str(N) +',3)\n')
+    txt_file.write(re.sub('( \[|\[|\])', '', str(S12)))
+    
+    txt_file.close()
+    print('Successfully converted to S parameters')
+    return
 
-#S11
-f.write('[\"' + portnames[0] + '\",\"' + portlocs[0] + '\"]\n')
-f.write('[\"' + portnames[1] + '\",\"' + portlocs[1] + '\"]\n')
-f.write('(\"' + portnames[0] + '\",\"' +str(polarization)+ '\",' +str(mode_id) + ',\"' + portnames[0] +'\",' + str(mode_id) +',\"transmission\")\n')
-f.write('('+ str(N) +',3)')
-f.close()
+convert_to_s_parameter(loss_model)
